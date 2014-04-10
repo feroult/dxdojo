c7b6257b6d928f7fde0b7c40e89ebde648edb4aa
Robison <rtesini@gmail.com>
2014-04-10 19:43:01 -0300

    perl

--- perl-test/ModuleTest.pl
+++ perl-test/ModuleTest.pl
@@ -2,9 +2,15 @@
 use strict;
 use warnings;
 use Test::More;
-use SimpleMath qw(add multiply);
+use SimpleMath qw(add multiply arrayExample hashExample);
  
 ok( add(6,7) == 13,                   'add' );
 ok( multiply(6,7) == 42,              'multiply' );
 
+arrayExample(1,2,3,4,5,6);
+
+hashExample();
+
 done_testing();
+
+
--- perl-test/SimpleMath.pm
+++ perl-test/SimpleMath.pm
@@ -1,10 +1,10 @@
 package SimpleMath;
 use strict;
 use warnings;
- 
+use Data::Dumper;
 use Exporter qw(import);
  
-our @EXPORT_OK = qw(add multiply);
+our @EXPORT_OK = qw(add multiply arrayExample hashExample);
  
 sub add {
   my ($x, $y) = @_;
@@ -14,6 +14,23 @@ sub add {
 sub multiply {
   my ($x, $y) = @_;
   return $x * $y;
+}
+
+sub arrayExample{
+	my @arr = @_;
+	for(@arr){
+		print ($_);
+	}
+	print "\n";
+}
+
+sub hashExample {
+	my %hash;
+	$hash{'1'} = 'Test 1';
+	$hash{'2'} = 'Test 2';
+	for(keys %hash){
+		print "$_ $hash{$_}\n";
+	}
 }
  
 1;
